ca98b7046ce4dece32552c9717a5e811752d26b1
lsps9150414 <lsps9150414@gmail.com>
2015-02-01 02:35:54 +0000

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -13,12 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20150131063023) do
 
-<<<<<<< HEAD
-=======
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
->>>>>>> ff4c6b276d40b1774c0d16a2f4babbfcc3ba31e2
   create_table "collect_dishes", force: :cascade do |t|
     t.integer  "dish_id"
     t.integer  "collection_id"
@@ -47,13 +41,8 @@ ActiveRecord::Schema.define(version: 20150131063023) do
     t.datetime "updated_at"
   end
 
-<<<<<<< HEAD
   add_index "comments", ["commentable_id", "commentable_type"], name: "index_comments_on_commentable_id_and_commentable_type"
   add_index "comments", ["user_id"], name: "index_comments_on_user_id"
-=======
-  add_index "comments", ["commentable_id", "commentable_type"], name: "index_comments_on_commentable_id_and_commentable_type", using: :btree
-  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
->>>>>>> ff4c6b276d40b1774c0d16a2f4babbfcc3ba31e2
 
   create_table "dishes", force: :cascade do |t|
     t.string   "store_name"
@@ -65,7 +54,6 @@ ActiveRecord::Schema.define(version: 20150131063023) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "fingerprint"
-<<<<<<< HEAD
   end
 
   create_table "restaurants", force: :cascade do |t|
@@ -91,44 +79,13 @@ ActiveRecord::Schema.define(version: 20150131063023) do
 
   add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
   add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context"
-=======
-  end
-
-  create_table "restaurants", force: :cascade do |t|
-    t.string   "name"
-    t.string   "latitude"
-    t.string   "longitude"
-    t.string   "photo"
-    t.string   "address"
-    t.string   "rating"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "taggings", force: :cascade do |t|
-    t.integer  "tag_id"
-    t.integer  "taggable_id"
-    t.string   "taggable_type"
-    t.integer  "tagger_id"
-    t.string   "tagger_type"
-    t.string   "context",       limit: 128
-    t.datetime "created_at"
-  end
-
-  add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true, using: :btree
-  add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context", using: :btree
->>>>>>> ff4c6b276d40b1774c0d16a2f4babbfcc3ba31e2
 
   create_table "tags", force: :cascade do |t|
     t.string  "name"
     t.integer "taggings_count", default: 0
   end
 
-<<<<<<< HEAD
   add_index "tags", ["name"], name: "index_tags_on_name", unique: true
-=======
-  add_index "tags", ["name"], name: "index_tags_on_name", unique: true, using: :btree
->>>>>>> ff4c6b276d40b1774c0d16a2f4babbfcc3ba31e2
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -149,28 +106,10 @@ ActiveRecord::Schema.define(version: 20150131063023) do
     t.datetime "updated_at"
     t.string   "avatar"
     t.string   "fingerprint"
-<<<<<<< HEAD
-=======
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
-  create_table "votes", force: :cascade do |t|
-    t.integer  "votable_id"
-    t.string   "votable_type"
-    t.integer  "voter_id"
-    t.string   "voter_type"
-    t.boolean  "vote_flag"
-    t.string   "vote_scope"
-    t.integer  "vote_weight"
-    t.datetime "created_at"
-    t.datetime "updated_at"
->>>>>>> ff4c6b276d40b1774c0d16a2f4babbfcc3ba31e2
-  end
-
-  add_index "votes", ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope", using: :btree
-  add_index "votes", ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope", using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
   create_table "votes", force: :cascade do |t|
     t.integer  "votable_id"
